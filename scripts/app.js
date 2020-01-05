8b60714a735df29daffa88d7cb336a619d87cf0b
ksrawr <kenneth.surban62@gmail.com>
2020-01-04 18:12:00 -0800

    adds game classes

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,54 @@
-console.log("me working");
+class Survivor {
+  constructor(image, x, y, vx, vy) {
+    this.image = image;
+    this.x = x;
+    this.y = y;
+    this.vx = vx;
+    this.vy = vy;
+    this.health = 40;
+    this.weapon = {
+      type: 'pistol',
+      image: "",
+      damage: ""
+    };
+  }
+
+  setImage(image) {
+    this.image = image;
+  }  
+}
+
+class Bullet {
+  constructor(image, x, y, vx, vy) {
+    this.image = image;
+    this.x = x;
+    this.y = y;
+    this.vx = vx;
+    this.vy = vy;
+  }
+}
+
+class Zombie {
+  constructor(image, x, y, vx, vy) {
+    this.image = image;
+    this.x = x;
+    this.y = y;
+    this.vx = vx;
+    this.vy = vy;
+    this.damage = 3;
+  }
+}
+
+class Game {
+  constructor() {
+    this.survivor = new Survivor();
+    this.zombies = [];
+    for(let i = 0; i < 10; i++) {
+      this.zombies.push(new Zombie());
+    }
+  }
+
+  startGame() {
+    
+  }
+}
