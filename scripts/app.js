d5b387631d339ce3f4ef406d88bb3c368984917a
ksrawr <kenneth.surban62@gmail.com>
2020-01-05 00:38:36 -0800

    creates function to find angle of mouse event

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,5 @@
 class Survivor {
-  constructor(image, x, y, vx, vy) {
+  constructor(image, x = 0, y = 0, vx, vy) {
     this.image = image;
     this.x = x;
     this.y = y;
@@ -56,12 +56,16 @@ class Game {
   }
 
   startGame() {
-    
+
   }
 }
 
 /* constants */
 const playerElement = document.getElementById('player');
+playerElement.style.left = "50%";
+playerElement.style.top = "50%";
+console.log(playerElement.offsetTop);
+console.log(playerElement.offsetLeft);
 
 const bodyElement = document.querySelector('body');
 
@@ -70,14 +74,38 @@ const mainElement = document.querySelector('main');
 const game = new Game();
 game.startGame();
 
+game.survivor.x = playerElement.offsetLeft;
+game.survivor.y = playerElement.offsetTop;
+
 /* --------- FUNCTIONS ------------ */
 
 
-/* --------EVENT LISTENERS ------- */
+// function calculateAngle(x, y, mouseClickedX, mouseClickedY) {
+//   // let angle = Math.atan(mouseClickedY - y, mouseClickedX - x) * 180 / Math.PI;
+//   let self = Math.sqrt(x ** 2 + y ** 2);
+//   // console.log(self);
+//   let mouse = Math.sqrt(mouseClickedX ** 2 + mouseClickedY ** 2);
+//   // console.log(mouse);
+//   let vectorProduct = x * mouseClickedX + y * mouseClickedY;
+//   // console.log(vectorProduct);
+//   let radians = Math.acos(vectorProduct/(self * mouse));
 
-let x = 0;
-let y = 0;
+//   return radians * (180/Math.PI);
+// }
+
+function calculateAngle(x, y, mouseClickedX, mouseClickedY) {
+  let angle =  Math.atan2(y, x) * 180 / Math.PI;
+
+  angle = (angle + 360) % 360;
+  // angle = 360 - angle;
+
+  return angle;
+}
+
+/* --------EVENT LISTENERS ------- */
 document.addEventListener('keydown', (event) => {
+  let x = game.survivor.x;
+  let y = game.survivor.y;
   if(event.key === 'ArrowDown' || event.key === 's') {
     y += 5;
     playerElement.style.top = y + 'px';
@@ -98,15 +126,28 @@ document.addEventListener('mousedown', (event) => {
 
   // const bulletTemplate = `<div id="bullet"></div>`;
   
-  const mouseClickedX = event.screenX;
-  const mouseClickedY = event.screenY;
+  const mouseClickedX = event.clientX;
+  const mouseClickedY = event.clientY;
+
+  let bulletX = game.survivor.x + 10;
+  let bulletY = game.survivor.y + 10;
   
   const bulletElement = document.createElement('div');
-  bulletElement.setAttribute("id", "bullet");
-  bulletElement.style.left = 10 + 'px';
-  bulletElement.style.top = 10 + 'px';
+  bulletElement.setAttribute("class", "bullet");
+  bulletElement.style.left = bulletX + 'px';
+  bulletElement.style.top = bulletY + 'px';
 
   mainElement.insertAdjacentElement('afterbegin', bulletElement);
 
+  /* Bullet should move until the it's reached the end of its trajectory */
+  /* remove bullet when it reaches the end */
+  const windowLimitX = game.documentWidth;
+  const windowLimitY = game.documentHeight;
+
+  const currentBullet = document.querySelector('.bullet');
+  // console.log(currentBullet);
+
+  const angle = calculateAngle(mouseClickedX - game.survivor.x, game.survivor.y - mouseClickedY);
+  console.log(angle);
   
 });
